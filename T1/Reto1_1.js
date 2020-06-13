ed577c4b55a88c870890b91d01c0a33246e05138
FabianCz95 <fabiancruz9504@gmail.com>
2020-06-12 19:55:36 -0500

    Tareas1

---
+++ T1/Reto1_1.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(0);
+  stroke(225)
+  line(200, 0, 200, 200);
+  line(200, 200, 0, 400);
+  line(200, 200, 400, 400);
+
+}
---
+++ T1/Reto1_3.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(250);
+  stroke(255,0,0)
+  strokeWeight(30);
+  noFill();
+  line(100,100,300,300);
+  ellipse(200,200,300);
+
+}
